4d0b45698b12e31b630166b40aa48aac19850703
NiTyAOP <134905791+NiTyAOP@users.noreply.github.com>
2023-10-17 20:41:41 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC9kP4LPoY7ZscwWcbN2L77q3XKZjV6vLY",
+    authDomain: "lets-chat-app-f031f.firebaseapp.com",
+    databaseURL: "https://lets-chat-app-f031f-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-app-f031f",
+    storageBucket: "lets-chat-app-f031f.appspot.com",
+    messagingSenderId: "209697985228",
+    appId: "1:209697985228:web:25a0fad7acd983a8c22869"
+  };
+  
+  // Initialize Firebase
+    firebase.initializeApp(firebaseConfig)
